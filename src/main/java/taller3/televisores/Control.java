f54755686bf28227e5777f3ebede831e277b4732
JPortoL <jporto@unal.edu.co>
2023-02-26 22:04:41 -0500

    pr1
    fe

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,37 @@
+package taller3.televisores;
+
+public class Control {
+private TV tv;
+public void setTV(TV tv) {
+	this.tv=tv;
+}
+public TV getTV() {
+	return tv;
+}
+public void turnOn() {
+	tv.turnOn();
+}
+public void turnOff() {
+	tv.turnOff();
+}
+public void canalUp() {
+	tv.canalUp();
+}
+public void canalDown() {
+	tv.canalDown();
+}
+public void volumenUp() {
+	tv.volumenUp();
+}
+public void volumenDown() {
+	tv.volumenDown();
+}
+public void setCanal(int canal) {
+	tv.canal=canal;
+}
+public void enlazar(TV tv) {
+	this.tv=tv;
+	tv.control=this;
+}
+
+}
